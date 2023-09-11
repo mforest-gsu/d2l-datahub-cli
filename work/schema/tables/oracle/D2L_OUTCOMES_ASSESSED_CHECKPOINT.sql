41d93664cd5842b6202fc7197467c5675bc4b0a4
Melody Forest <mforest@gsu.edu>
2023-09-11 11:13:57 -0400

    All non-primary key fields now default to null; DATE fields are now TIMESTAMP fields; Decimal NUMBER fields are now DECIMAL fields

--- work/schema/tables/oracle/D2L_OUTCOMES_ASSESSED_CHECKPOINT.sql
+++ work/schema/tables/oracle/D2L_OUTCOMES_ASSESSED_CHECKPOINT.sql
@@ -1,17 +1,17 @@
 DROP TABLE D2L_OUTCOMES_ASSESSED_CHECKPOINT;
 CREATE TABLE D2L_OUTCOMES_ASSESSED_CHECKPOINT (
-  CheckpointId VARCHAR2(16) NOT NULL,
-  DemonstrationId VARCHAR2(16) NOT NULL,
-  Feedback NVARCHAR2(1000) NOT NULL,
-  ConfigDecayRate NUMBER(10) NOT NULL,
-  ConfigAggregationMethod NUMBER(10) NOT NULL,
-  ConfigIncludedActivities NUMBER(10) NOT NULL,
-  ConfigMultipleAttemptStrategy NUMBER(10) NOT NULL,
-  ConfigRecentlyAssessedActivityCount NUMBER(10) NOT NULL,
-  ConfigTieBreaker NUMBER(10) NOT NULL,
-  LastModifiedDate DATE DEFAULT NULL,
+  CheckpointId VARCHAR2(36) NOT NULL,
+  DemonstrationId VARCHAR2(36) NOT NULL,
+  Feedback NVARCHAR2(1250) DEFAULT NULL,
+  ConfigDecayRate NUMBER(10) DEFAULT NULL,
+  ConfigAggregationMethod NUMBER(10) DEFAULT NULL,
+  ConfigIncludedActivities NUMBER(10) DEFAULT NULL,
+  ConfigMultipleAttemptStrategy NUMBER(10) DEFAULT NULL,
+  ConfigRecentlyAssessedActivityCount NUMBER(10) DEFAULT NULL,
+  ConfigTieBreaker NUMBER(10) DEFAULT NULL,
+  LastModifiedDate TIMESTAMP WITH LOCAL TIME ZONE DEFAULT NULL,
   LastModifiedBy NUMBER(10) DEFAULT NULL,
-  IsDeleted NUMBER(1) NOT NULL
+  IsDeleted NUMBER(1) DEFAULT NULL
 );
 CREATE UNIQUE INDEX D2L_OUTCOMES_ASSESSED_CHECKPOINT_PK ON D2L_OUTCOMES_ASSESSED_CHECKPOINT (CheckpointId,DemonstrationId);
 QUIT;
