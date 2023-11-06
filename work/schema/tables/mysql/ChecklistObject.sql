c87ffbf29ab7995e9d8fea9efd9789f3bfb8638c
Melody Forest <mforest@gsu.edu>
2023-11-06 10:51:29 -0500

    November 2023 updates

--- work/schema/tables/mysql/ChecklistObject.sql
+++ work/schema/tables/mysql/ChecklistObject.sql
@@ -11,5 +11,7 @@ CREATE TABLE `ChecklistObject` (
   `SortOrder` INT DEFAULT NULL,
   `Version` BIGINT DEFAULT NULL,
   `ResultId` INT DEFAULT NULL,
+  `DateDeleted` DATETIME DEFAULT NULL,
+  `DeletedBy` INT DEFAULT NULL,
   UNIQUE KEY (`ChecklistId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
